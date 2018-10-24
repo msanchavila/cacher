42251cb5ddb1e2777813a86054f8489919695a37
Martin Sanchez <msanchavila@gmail.com>
2018-10-24 11:50:34 -0700

    adding setup to install as module

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='caching',
+  version='0.1',
+  url='https://github.com/msanchavila/caching'
+  install_requires=['Pandas'],
+  project_urls={  # Optional
+    'Bug Reports': 'https://github.com/msanchavila/caching/issues',
+  },
+)
